bc2572486107705bf8381128f7e6bb9715fa9f5d
Ewerton <ewertonfiamoncini20@gmail.com>
2025-09-24 10:57:55 -0300

    Refactor variable declarations to block scope for user data logging

--- javascript/dados.js
+++ javascript/dados.js
@@ -1,10 +1,25 @@
-var name = 'Ewerton F.'
-var age = 25
-var wage = 6080
-var overtime = 1320
+{
+let name = 'Ewerton F.'
+let age = 25
+let wage = 6080
+let overtime = 1320
 
 console.log('Nome:', name);
 console.log('Idade:', age,'anos');
 console.log('Salário: R$',wage);
 console.log('Hora extra: R$',overtime);
 console.log('Renda total: R$',wage + overtime);
+}
+
+{
+name = 'Maria F.'
+age = 25
+wage = 4560
+overtime = 1230
+
+console.log('Nome:', name);
+console.log('Idade:', age,'anos');
+console.log('Salário: R$',wage);
+console.log('Hora extra: R$',overtime);
+console.log('Renda total: R$',wage + overtime);
+}
