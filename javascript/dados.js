87c7115077f088613357fdfc614fdd6e3537a9c8
Ewerton <ewertonfiamoncini20@gmail.com>
2025-09-24 10:55:07 -0300

    Add initial implementation of data logging for user details

---
+++ javascript/dados.js
@@ -0,0 +1,10 @@
+var name = 'Ewerton F.'
+var age = 25
+var wage = 6080
+var overtime = 1320
+
+console.log('Nome:', name);
+console.log('Idade:', age,'anos');
+console.log('Salário: R$',wage);
+console.log('Hora extra: R$',overtime);
+console.log('Renda total: R$',wage + overtime);
