97350c5a83836cb872ebd36df2d0c60c86111f33
Ewerton <ewertonfiamoncini20@gmail.com>
2025-09-25 12:41:50 -0300

    Add initial implementation of variable declarations and type logging in tipagemFraca.js

---
+++ javascript/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal';
+
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 1.4534;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+//evitar escolher nomes genericos e siglas 
+let valor = '';
+let numero = 1;
+let pqp = false; //produto quimico perigoso... kkk
