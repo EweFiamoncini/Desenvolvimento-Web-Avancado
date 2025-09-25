1ca9882c3b756e8c55acff1d50d613179eeb08c5
Ewerton <ewertonfiamoncini20@gmail.com>
2025-09-25 12:33:54 -0300

    Refactor variable declarations and enhance comments for clarity in variaveisEConstantes.js

--- javascript/variaveisEConstantes.js
+++ javascript/variaveisEConstantes.js
@@ -1,3 +1,13 @@
 var a = 3;
 let b = 4;
+
+//antes de usar o var use  o let pois o var pode ser redeclarado sem que acontecça nenhum erro no codigo.
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+//usar sempre const para declarar uma variavel constante.
 const c = 5;
+
+console.log(c);
