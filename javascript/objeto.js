1b87f5ca1a588cb1703c0262973b2dcf204c17fd
Ewerton <ewertonfiamoncini20@gmail.com>
2025-09-27 09:01:35 -0300

    Add initial implementation of array, boolean, and object examples in array.js, booleanos.js, and objeto.js

---
+++ javascript/objeto.js
@@ -0,0 +1,11 @@
+const prod1 = {};
+prod1.nome = 'Celular ultra mega';
+prod1.preco = 4544.90;
+prod1['desconto legal'] = 0.40; //evitar este tipo de atributo
+console.log(prod1);
+
+const prod2 = {
+  nome: 'Teclado Gamer',
+  preco: 212.33
+};
+console.log(prod2);
