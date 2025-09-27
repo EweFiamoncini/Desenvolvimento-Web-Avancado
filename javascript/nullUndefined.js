a1952354fec6eba98ef8a3e8f60b96d1ca2a2ea6
Ewerton <ewertonfiamoncini20@gmail.com>
2025-09-27 09:13:51 -0300

    Add initial implementation of null and undefined examples in nullUndefined.js

---
+++ javascript/nullUndefined.js
@@ -0,0 +1,20 @@
+let valor; //não inicializada
+console.log(valor);
+
+valor = null; //ausencia de valor
+console.log(valor);
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.preco = 3.50;
+console.log(produto);
+
+produto.preco = undefined;//evite ateribuir undefined
+console.log(!!produto.preco);
+console.log(produto);
+
+produto.preco = null; //sem preco
+console.log(!!produto.preco);
+console.log(produto);
